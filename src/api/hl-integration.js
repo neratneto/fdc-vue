95301acac2a8194a2fa38f79e963b32dcf142538
Renato (neratneto) <nerat.neto@gmail.com>
2020-10-18 02:28:47 -0300

    hlapikey encrypted

--- src/api/hl-integration.js
+++ src/api/hl-integration.js
@@ -1,7 +1,11 @@
 import axios from 'axios'
  
+const secrets = {
+  a: window.localStorage.getItem("apiKey"),
+  b: window.localStorage.getItem("clientId")
+}
 const HLApi = {
-    apiKey: window.localStorage.getItem("hlApiKey"),
+    apiKey: `d${b[11]}6d${b[1]}b5c-${b[11]}${b[3]}55-${b[9]}fd5-${b[4]}ef${b[1]}-ffdc${b[11]}e${b[11]}${b[6]}${b[6]}${b[0]}f${b[2]}`,
     contactsUrl: 'https://rest.gohighlevel.com/v1/contacts',
     zapierUrl: 'https://msgsndr.com/zapier'
   };
